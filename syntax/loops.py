2705506f8547057e05fa9549ab39564e794edce0
Gaurav Shukla <gauravshukla2006ldh@gmail.com>
2026-04-24 17:40:49 +0530

    loops

---
+++ syntax/loops.py
@@ -0,0 +1,18 @@
+#this consist of basic loop program
+def itr1() :
+    students = ["Gaurav","Gurmeet","Devansh","Udit","Harshit"]
+    for student in students :
+        print(student)
+def itr2() :
+    for i in range (11):
+        print(i)
+
+def itr3() :
+    count = 0
+    while count<5:
+        print(count)
+        count= count + 1
+
+
+
+
